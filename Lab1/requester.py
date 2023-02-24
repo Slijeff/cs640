f54a081e17042fd9fd57d3d637f6fc1b2ea2ab1a
Jeffrey Hui <jeff83636tx@gmail.com>
2023-02-24 00:08:49 -0600

    feat: initial send request

--- Lab1/requester.py
+++ Lab1/requester.py
@@ -1,6 +1,7 @@
 import argparse
 import socket
 from collections import defaultdict
+import struct
 
 
 class Requester:
@@ -12,18 +13,31 @@ class Requester:
         self.filename = filename
         self.tracker_info = self.read_tracker()
 
-    def read_tracker(self) -> defaultdict:
+    def read_tracker(self) -> defaultdict[list]:
         info = defaultdict(list)
         with open("tracker.txt", "r") as f:
             for line in f:
                 line = line.replace("\n", "").split(" ")
-                info[line[0]].append((int(line[1]), line[2], line[3]))
+                info[line[0]].append(
+                    (int(line[1]), socket.gethostbyname(line[2]), int(line[3]))
+                )
 
         # format: {filename: [(ID, hostname, port), (ID, hostname, port)]} sorted by ID
         for v in info.values():
             v.sort(key=lambda x: x[0])
 
         return info
+
+    def send_request(self) -> None:
+        header = struct.pack("!cqq", "R".encode(), 0, 0)
+        # Hard coded for now, need to have a for-loop to retrieve different parts
+        self.sock.sendto(
+            header + self.filename.encode(),
+            (
+                self.tracker_info[self.filename][0][1],
+                self.tracker_info[self.filename][0][2],
+            ),
+        )
 
 
 if __name__ == "__main__":
@@ -37,3 +51,4 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     requester = Requester(args.p, args.o)
+    requester.send_request()
