a3ed3d51e9ba8a34a4615083e1888327d8da6f56
Jeffrey Hui <jeff83636tx@gmail.com>
2023-03-22 12:20:53 -0500

    feat: add network queue

--- Lab2/emulator.py
+++ Lab2/emulator.py
@@ -1,6 +1,33 @@
 import socket
 import argparse
+import struct
 from typing import List, Tuple, Union
+from collections import deque
+
+Table_Entry = Tuple[Tuple, Tuple, int, int]
+STRUCT_FORMAT = "!cIHIHI"
+
+
+# Write our own wrapper class for the queue
+class NetworkQueue:
+    def __init__(self, queue_size: int) -> None:
+        self.queue_size = queue_size
+        self.queue = deque()
+
+    def __len__(self) -> int:
+        return len(self.queue)
+
+    def enqueue(self, packet: bytes) -> None:
+        if len(self.queue) < self.queue_size:
+            self.queue.appendleft(packet)
+        else:
+            raise Exception("Queue is full")
+
+    def dequeue(self) -> Union[bytes, None]:
+        if len(self.queue) > 0:
+            return self.queue.pop()
+        else:
+            return None
 
 
 class Emulator:
@@ -12,27 +39,42 @@ class Emulator:
         self.queue_size = queue_size
         self.log_name = log_name
 
-        dest, next_hop, delay, prob = self.read_forwarding_table()
-        print(dest, next_hop, delay, prob)
+        # format: (destination, next_hop, delay, loss_prob)
+        self.forwarding_table = self.read_forwarding_table()
 
-    def read_forwarding_table(self) -> Tuple[Tuple, Tuple, int, int]:
-        destination = next_hop = delay = loss_prob = None
+        self.high_priority_queue = NetworkQueue(self.queue_size)
+        self.medium_priority_queue = NetworkQueue(self.queue_size)
+        self.low_priority_queue = NetworkQueue(self.queue_size)
+
+    def read_forwarding_table(self) -> List[Table_Entry]:
+        entries: List[Table_Entry] = []
         # read the forwarding table
         with open(self.filename, "r") as f:
             for line in f:
                 # Find our own emulator
                 # TODO: not sure if this is the right way to do it
-                if line and socket.gethostbyname(line.split(" ")[0]) == "127.0.0.1":
+                if (
+                    line
+                    and socket.gethostbyname(line.split(" ")[0]) == "127.0.0.1"
+                    and line.split(" ")[1] == str(self.port)
+                ):
                     line = line.split(" ")
                     destination = (line[2], line[3])
                     next_hop = (line[4], line[5])
                     delay = int(line[6])
                     loss_prob = int(line[7])
-                    break
+                    entries.append((destination, next_hop, delay, loss_prob))
                 else:
                     continue
+        return entries
 
-        return destination, next_hop, delay, loss_prob
+    def route_packet(self, incoming_packet: bytes) -> None:
+        # unpack the packet
+        header = incoming_packet[:17]
+        payload = incoming_packet[17:]
+        priority, src_addr, src_port, dest_addr, dest_port, length = struct.unpack(
+            STRUCT_FORMAT, header
+        )
 
 
 if __name__ == "__main__":
