1dd4a54f636044aed56c19ad48591ee7a6eb217e
Jeffrey Hui <jeff83636tx@gmail.com>
2023-03-26 21:57:19 -0500

    Update: complete basic routing logic

--- Lab2/emulator.py
+++ Lab2/emulator.py
@@ -3,8 +3,11 @@ import argparse
 import struct
 from typing import List, Tuple, Union
 from collections import deque
+from time import time
 
+Address = Tuple[str, int]
 Table_Entry = Tuple[Tuple, Tuple, int, int]
+Queue_Entry = Tuple[bytes, float, Address, Address]
 STRUCT_FORMAT = "!cIHIHI"
 
 
@@ -17,17 +20,21 @@ class NetworkQueue:
     def __len__(self) -> int:
         return len(self.queue)
 
-    def enqueue(self, packet: bytes) -> None:
+    def enqueue(self, packet: bytes, dest: Address, hop: Address) -> None:
         if len(self.queue) < self.queue_size:
-            self.queue.appendleft(packet)
+            # includes current time when enqueuing in miliseconds
+            self.queue.appendleft((packet, time() * 1000, dest, hop))
         else:
             raise Exception("Queue is full")
 
-    def dequeue(self) -> Union[bytes, None]:
+    def dequeue(self) -> Union[Queue_Entry, None]:
         if len(self.queue) > 0:
             return self.queue.pop()
         else:
             return None
+
+    def peek(self) -> Union[Queue_Entry, None]:
+        return self.queue[-1] if len(self.queue) > 0 else None
 
 
 class Emulator:
@@ -43,7 +50,8 @@ class Emulator:
         self.sock.bind((self.UDP_IP, self.port))
         self.sock.settimeout(120)
         # making it non-blocking
-        self.sock.setblocking(0)
+        self.sock.setblocking(False)
+        self.currently_delaying: Queue_Entry = None
 
         # format: (destination, next_hop, delay, loss_prob)
         self.forwarding_table = self.read_forwarding_table()
@@ -61,12 +69,12 @@ class Emulator:
                 # TODO: not sure if this is the right way to do it
                 if (
                     line
-                    and socket.gethostbyname(line.split(" ")[0]) == "127.0.0.1"
+                    and socket.gethostname() == line.split(" ")[0]
                     and line.split(" ")[1] == str(self.port)
                 ):
                     line = line.split(" ")
-                    destination = (line[2], line[3])
-                    next_hop = (line[4], line[5])
+                    destination = (line[2], int(line[3]))
+                    next_hop = (line[4], int(line[5]))
                     delay = int(line[6])
                     loss_prob = int(line[7])
                     entries.append((destination, next_hop, delay, loss_prob))
@@ -75,30 +83,58 @@ class Emulator:
         return entries
 
     def route_packet(self, incoming_packet: bytes) -> None:
-        
+
         # unpack the packet
         header = incoming_packet[:17]
         payload = incoming_packet[17:]
         priority, src_addr, src_port, dest_addr, dest_port, length = struct.unpack(
             STRUCT_FORMAT, header
         )
+
+        destination = (dest_addr, int(dest_port))
+
+        if not self.lookup_by_destination(destination):
+            # TODO: logging when destination not in forwarding_table
+            pass
+
         try:
             if priority == 1:
-                self.high_priority_queue.enqueue(incoming_packet)
+                self.high_priority_queue.enqueue(
+                    incoming_packet, destination, self.lookup_by_destination(destination))
             elif priority == 2:
-                self.medium_priority_queue.enqueue(incoming_packet)
+                self.medium_priority_queue.enqueue(
+                    incoming_packet, destination, self.lookup_by_destination(destination))
             elif priority == 3:
-                self.low_priority_queue.enqueue(incoming_packet)
+                self.low_priority_queue.enqueue(
+                    incoming_packet, destination, self.lookup_by_destination(destination))
         except:
             # TODO: logging when queue is full
             pass
-        
 
+        # get a packet from the queues if there's no currently delayed packet
+        if not self.currently_delaying:
+            for Q in [self.high_priority_queue, self.medium_priority_queue, self.low_priority_queue]:
+                if Q.peek():
+                    self.currently_delaying = Q.dequeue()
+                    break
+        else:
+            # decide if the delay is over
+            if time() * 1000 - self.currently_delaying[1] <= self.lookup_by_destination(self.currently_delaying[2]):
+                # forward
+                self.sock.sendto(self.currently_delaying[0], self.currently_delaying[3])
+                self.currently_delaying = None
+
+    def lookup_by_destination(self, destination: Address) -> Union[Table_Entry, None]:
+        for e in self.forwarding_table:
+            if destination == e[0]:
+                return e
+        return None
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Network Emulator")
-    parser.add_argument("-p", help="the port of the emulator", type=int, required=True)
+    parser.add_argument("-p", help="the port of the emulator",
+                        type=int, required=True)
     parser.add_argument(
         "-q", help="the size of each of the three queues", type=int, required=True
     )
@@ -108,7 +144,8 @@ if __name__ == "__main__":
         type=str,
         required=True,
     )
-    parser.add_argument("-l", help="the name of the log file", type=str, required=True)
+    parser.add_argument("-l", help="the name of the log file",
+                        type=str, required=True)
 
     args = parser.parse_args()
     # initialize Emulator
