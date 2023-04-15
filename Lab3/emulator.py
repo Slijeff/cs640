7934894016ceb4730cf593dfb8e451483e3259a9
Jeffrey Hui <jeff83636tx@gmail.com>
2023-04-14 22:30:52 -0500

    Update emulator.py

--- Lab3/emulator.py
+++ Lab3/emulator.py
@@ -1,24 +1,44 @@
 import socket
-import time
+from dataclasses import dataclass
+import pickle
+# import time
 import argparse
+from typing import Dict, List, Optional, Tuple, Literal, Union
 
+Address = Tuple[str, int]
+LinkState = Tuple[Address, int] # address and cost to that address
+
+@dataclass
+class Message:
+    creator: Address
+    packet_type: Literal["HELLO", "LSM"]
+    seq_num: Optional[int] = None
+    ttl: Optional[int] = None
+    neighbors: Optional[List[LinkState]] = None 
+
+
+    def to_bytes(self) -> bytes:
+        return pickle.dumps(self)
 
 class Emulator():
     def __init__(self, port: int, filename: str) -> None:
         self.port = port
         self.topo_file = filename
         
+        self.neighbors: List[Address] = [] # a list of (ip, port) pair that indicates the neighbors
+       
         self.ip = socket.gethostbyname(socket.gethostname())
-        
-        self.neighbors = [] # a list of (ip, port) pair that indicates the neighbors
-        
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind((self.ip, self.port))
         self.sock.setblocking(False)
+        
+        self.lsp_interval = 0.5 # in seconds
+        self.forwarding_table: Dict[Address, Address] = {} # maps (dst_addr, dst_ip) to (next_hop_addr, next_hop_ip)
 
         self.read_topology()
         
-    def read_topology(self):
+    def read_topology(self) -> List[Address]:
+        topology = [] 
         with open(self.topo_file, 'r') as f:
             for line in f:
                 # find itself in the topo_file
@@ -26,9 +46,13 @@ class Emulator():
                 line = [x.replace('\n', '').split(',') for x in line]
                 line = [(x[0], int(x[1])) for x in line]
                 if line[0][0] == self.ip and line[0][1] == self.port:
-                    self.neighbors = line[1:]
-
-        print(self.neighbors)
+                    topology = line[1:]
+        
+        assert topology != []
+        return topology
+    
+    def send_msg(self, msg: Message, dest: Address):
+        self.sock.sendto(msg.to_bytes(), dest)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Network Emulator")
@@ -39,6 +63,5 @@ if __name__ == "__main__":
     )
 
     arg = parser.parse_args()
-    Emulator(arg.p, arg.f)
-
+    e = Emulator(arg.p, arg.f)
 
