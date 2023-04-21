d0e0bf262595a3d5661a9db9eb3bb771e8aecd0f
Jeffrey Hui <jeff83636tx@gmail.com>
2023-04-20 19:59:44 -0500

    update emulator.py

--- Lab3/emulator.py
+++ Lab3/emulator.py
@@ -1,13 +1,12 @@
 import socket
 from dataclasses import dataclass
 import pickle
-# import time
+import time
 import argparse
 from typing import Dict, List, Optional, Set, Tuple, Literal 
 from collections import defaultdict, deque
 
 Address = Tuple[str, int]
-LinkState = Tuple[Address, int] # address and cost to that address
 
 @dataclass
 class Message:
@@ -15,11 +14,41 @@ class Message:
     packet_type: Literal["HELLO", "LSM", "TRACE"]
     seq_num: Optional[int] = None
     ttl: Optional[int] = None
-    neighbors: Optional[List[LinkState]] = None 
-
+    # since the cost is always 1, we only send adjacent nodes
+    neighbors: Optional[Set[Address]] = None 
 
     def to_bytes(self) -> bytes:
         return pickle.dumps(self)
+
+class NeighborList():
+    def __init__(self, neighbors: Set[Address], timeout: float) -> None:
+        self.neighbors: List[Tuple[Address, float]] = []
+        self.timeout = timeout
+        
+        for n in neighbors:
+            self.neighbors.append((n, time.time()))
+
+    def record_neighbor(self, neighbor: Address) -> Tuple[Address, Literal["UP"]] | None:
+        if neighbor not in self.neighbors:
+            # a neighbor is online
+            self.neighbors.append((neighbor, time.time()))
+            return (neighbor, 'UP')
+        else:
+            old = None
+            for entry in self.neighbors:
+                if entry[0] == neighbor:
+                    old = entry
+            assert old != None
+            self.neighbors.remove(old)
+            self.neighbors.append((neighbor, time.time()))
+    
+    def check_timeout(self) -> Tuple[Address, Literal["DOWN"]] | None:
+        for entry in self.neighbors:
+            if time.time() - entry[1] > self.timeout:
+                self.neighbors.remove(entry)
+                return (entry[0], "DOWN")
+
+
 
 class Emulator():
     def __init__(self, port: int, filename: str) -> None:
@@ -38,14 +67,17 @@ class Emulator():
         
         self.address = (self.ip, self.port)
         self.lsp_interval = 0.5 # in seconds
+        self.hello_interval = 0.5 # timeout for neighbor nodes
         self.forwarding_table: Dict[Address, Address] = {} # maps (dst_addr, dst_ip) to (next_hop_addr, next_hop_ip)
         self.all_nodes_except_self: List[Address] = []
         self.adj_list: Dict[Address, Set[Address]] = defaultdict(set)
+        
 
         self.read_topology()
+        self.neighbor_list = NeighborList(set(self.neighbors), self.hello_interval)
         self.build_forwarding_table()
         
-    def read_topology(self) -> List[Address]:
+    def read_topology(self) -> None:
         topology = [] 
         with open(self.topo_file, 'r') as f:
             for line in f:
@@ -56,15 +88,31 @@ class Emulator():
                 # add to adjacency list
                 self.adj_list[line[0]] |= set(line[1:])
                 # add to topology
-                if line[0][0] == self.ip and line[0][1] == self.port:
+                if not topology and line[0][0] == self.ip and line[0][1] == self.port:
                     topology = line[1:]
                 # record all other nodes in the network except itself 
                 else:
                     self.all_nodes_except_self.append(line[0])
         
         assert topology != []
-        return topology
+        self.neighbors = topology
     
+    def update_adj_list(self, msg: Message) -> bool:
+        """
+        returns True if adjacency list changed, False if nothing changed
+        """
+        assert msg.creator in self.adj_list, "Source node not in topology file"
+
+        old = self.adj_list[msg.creator]
+        new = msg.neighbors
+
+        if new:
+            if old == new:
+                return False
+            else:
+                self.adj_list[msg.creator] = new
+
+        return True
 
     def build_forwarding_table(self) -> None:
         for node in self.all_nodes_except_self:
@@ -90,6 +138,23 @@ class Emulator():
     def send_msg(self, msg: Message, dest: Address):
         # self.sock.sendto(msg.to_bytes(), dest)
         pass
+    
+    def emulate_once(self, msg: Message) -> None:
+        # If message is HELLO
+        if msg.packet_type == "HELLO":
+            self.neighbor_list.record_neighbor(msg.creator)
+        #If message is Link State
+        elif msg.packet_type == "LSM":
+            pass
+        # If message is Traceroute
+        elif msg.packet_type == "TRACE":
+            pass
+
+        # Check if any neighbor is dead
+        result = self.neighbor_list.check_timeout()
+        if result:
+            # Send new link state message and rebuild forwarding_table
+            pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Network Emulator")
